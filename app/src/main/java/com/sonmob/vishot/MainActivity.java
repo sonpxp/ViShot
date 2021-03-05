262bdb4922bc4bdf4c38c526087077b5d10da441
sonpxp <sonmob202@gmail.com>
2021-03-05 14:52:49 +0700

    implement viewbinding, setup gallery intent

--- app/src/main/java/com/sonmob/vishot/MainActivity.java
+++ app/src/main/java/com/sonmob/vishot/MainActivity.java
@@ -1,23 +1,41 @@
 package com.sonmob.vishot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sonmob.vishot.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+    private int IMAGE_REQUEST_CODE = 12;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
 
 
+        mainBinding.edEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, IMAGE_REQUEST_CODE);
+            }
+        });
+    }
 
     }
 }
